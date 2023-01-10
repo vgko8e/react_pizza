d1aa51ee1042c82ccb144264961799e3c71bcb3e
vgko8e <92440969+vgko8e@users.noreply.github.com>
2023-01-10 22:35:40 +0200

    comp

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+const Categories = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">All</li>
+        <li>Meat</li>
+        <li>Vegetarians</li>
+        <li>Grill</li>
+        <li>Spicy</li>
+        <li>Closed</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;

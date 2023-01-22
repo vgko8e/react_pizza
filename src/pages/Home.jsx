22a02fff1ba137ed37257f63b7242b53cb3ad8ec
vgko8e <92440969+vgko8e@users.noreply.github.com>
2023-01-22 17:55:17 +0200

    router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+
+const Home = () => {
+  const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://my.api.mockaroo.com/pizzas.json?key=5fcbe6c0")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPizzas(data);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">All pizzas</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
+          : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;

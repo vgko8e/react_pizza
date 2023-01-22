408f5b4834182bb6f71ffdf666abb3fc7a37113a
vgko8e <92440969+vgko8e@users.noreply.github.com>
2023-01-22 17:54:14 +0200

    Router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 import "./scss/app.scss";
-import { useState, useEffect } from "react";
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import PizzaBlock from "./components/PizzaBlock";
-import Skeleton from "./components/PizzaBlock/Skeleton";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 function App() {
-  const [pizzas, setPizzas] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("https://my.api.mockaroo.com/pizzas.json?key=5fcbe6c0")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setPizzas(data);
-        setIsLoading(false);
-      });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">All pizzas</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
-              : pizzas.map((pizza) => <PizzaBlock key={pizza.id} {...pizza} />)}
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Route>
+        </Routes>
       </div>
     </div>
   );
